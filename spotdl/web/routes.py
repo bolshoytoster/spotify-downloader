6b5c91d586cad7c2c6d216d78d4dd9bbfa3bde7a
Tzur Soffer <tzur.soffer@gmail.com>
2026-05-17 16:00:57 -0700

    Black + isort

--- spotdl/web/routes.py
+++ spotdl/web/routes.py
@@ -3,8 +3,8 @@ Module which contains the web client routes and functions.
 """
 
 import asyncio
-import uuid
 import os
+import uuid
 from typing import Any, Optional, cast
 
 # from datastar_py.sse import DatastarEvent
@@ -29,7 +29,9 @@ __all__ = ["router"]
 
 router = APIRouter()
 
-templates = Jinja2Templates(directory=os.path.dirname(os.path.realpath(__file__)) + "/components")
+templates = Jinja2Templates(
+    directory=os.path.dirname(os.path.realpath(__file__)) + "/components"
+)
 
 
 # PATHS
