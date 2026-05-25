3d26aa7075a14ec46ebc8e1d3f6cab7f7f36d7c2
bolshoytoster <toasterbig@gmail.com>
2026-05-25 20:22:11 +0100

    Use requests' default User-Agent for AZLyrics

--- spotdl/providers/lyrics/azlyrics.py
+++ spotdl/providers/lyrics/azlyrics.py
@@ -27,10 +27,6 @@ class AzLyrics(LyricsProvider):
         self.session.headers.update(
             {
                 "Host": "www.azlyrics.com",
-                "User-Agent": (
-                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
-                    "(KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36"
-                ),
                 "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
                 "Accept-Language": "en-US,en;q=0.5",
                 "Accept-Encoding": "gzip, deflate, br, zstd",
